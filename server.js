db8f2dce2aea983ba29b604787c22043edcde3f8
sirdmon <sirdmon@di.com>
2022-02-21 21:20:07 +0100

    david's stuff

--- server.js
+++ server.js
@@ -2,77 +2,62 @@ const express = require('express')
 const app = express();
 const bodyParser= require('body-parser')
 app.use(bodyParser.json()); 
+const fetch = require('cross-fetch')
 
-import { getUserList } from './user';
-const userList = getUserList();
 
-app.get('/users', (req, res) => {
 
-    return res.status(200).send({
-        success: 'true',
-        message: 'users',
-        users: userList
+/**
+ * This endpoint returns user cups from user data (NIF)
+ * @returns cups 
+ */
+app.get('/supplies', async (req, res) => {
+
+    const {username, password, nif} = req.query
+
+    const passwordQueryURL = encodeURIComponent(password)
+    const fetchURL = `https://datadis.es/nikola-auth/tokens/login?username=${username}&password=${passwordQueryURL}`
+    const response = await fetchFromDatadis(fetchURL, "POST")
+    if(!response.ok){
+        return res.status(400).send({error: response.error})
+    }
+
+    const token = await response.text()
+
+    const fetchSupplyURL = `https://datadis.es/api-private/api/get-supplies?authorizedNif=${nif}`
+
+    const supplyResponse = await fetch(fetchSupplyURL, {
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+
     })
 
+
+    if(!supplyResponse.ok){
+        return res.status(400).send({error: response.error})
+    }
+
+
+    const supplies = await supplyResponse.json()
+
+
+
+    //obtain consumption from DATADIS given supplies
+
+    return res.status(200).json(supplies)
+
+
+    
+
 });
+
+async function fetchFromDatadis(url, method){
+    return fetch(url, {method})
+}
 
 app.listen(8000, () => {
     console.log('Port 8000')
 });
-
-app.post('/addUser', (req, res) => {
-    if (req.body.name) {
-        return res.status(400).send({
-            success: "false",
-            message: "name is required",
-        });
-    } else if (req.body.companies) {
-        return res.status(400).send({
-            succes: "false",
-            message: "companies is required",
-        });
-    }
-    const user = {
-        id: userList.length + 1,
-        isPublic: req.body.isPublic,
-        name: req.body.name,
-        companies: req.body.companies,
-        books: req.body.books
-    };
-
-    userList.push(user); return res.status(201).send({
-        success: "true",
-        message: "user added succesfully",
-        user,
-
-    });
-});
-app.put('/updateUser/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    console.log("id:",id)
-    console.log("body",req.body)
-    const updatedUser = {
-        id: id,
-        isPublic: req.body.isPublic,
-        name: req.body.name,
-        companies: req.body.companies,
-        books: req.body.books,
-    };
-    for (let i = 0; i < userList.length; i++) {
-        if (userList[i].id === id) {
-            userList[i] = updatedUser;
-            return res.status(201).send({
-                success: "true",
-                message: "user added successfully",
-                updatedUser
-            });
-        }
-    }
-    return res.status(404).send({
-        success: "true",
-        message: "error in update",
-    });
-})
 
 
 
